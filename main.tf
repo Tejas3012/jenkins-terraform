339c144aaf2617b99ebe2c4f5c40d120f884f84d
Tejas Ramesh Chitte <145746716+Tejas3012@users.noreply.github.com>
2025-06-12 18:31:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-ource "aws_instance" "vm" {
+resource "aws_instance" "vm" {
     ami = "ami-05ab12222a9f39021"    #replace ami id
     instance_type = "t2.micro"
     key_name = "laptop-key"           # key pair name also
